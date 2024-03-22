a56ddf8b9c9cf1afd74f3d4c85c1e904f888fa82
Giorgio Fabbroni <giorgiofabbroni@hotmail.com>
2024-03-22 18:21:39 +0100

    created 2 out 6 functions requested, and also added a main method to my class

--- ExamUnit3.cs
+++ ExamUnit3.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+public class ExamUnit3
+{
+    public int Square(int num)
+    {
+        return num * num;
+    }
+
+    public double InchesToMillimeters(double inches)
+    {
+        return inches * 25.4;
+    }
+
+    public static void Main(string[] args)
+    {
+      //testing the methods
+        ExamUnit3 exam = new ExamUnit3();
+        Console.WriteLine(exam.Square(5));
+        Console.WriteLine(exam.InchesToMillimeters(5));
+    }
+}
